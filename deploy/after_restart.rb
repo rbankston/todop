3bd00eec33876b73e526584cc700a6f9acb289e6
rbankston <rbankston@engineyard.com>
2017-02-24 13:09:55 -0500

    added an rsync

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,4 +1,4 @@
 run! "cd #{config.latest_release} && MIX_ENV=prod mix deps.get && MIX_ENV=prod mix release"
-
+run "rsync -avHl #{config.latest_release}/rel #{config.shared}/rel"
 
 sudo "monit restart phoenix_#{config.app}"
